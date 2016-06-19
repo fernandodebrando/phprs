e385cce352fabdc069ad91f2aa817247e41f49d0
Fernando <fernando.debrando@gmail.com>
2016-06-19 15:13:08 -0300

    Adicionando conexão com MySQL

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
-mysqli_connect("l0eu1g9mu7b750ur", "ynq3czogl6mtuao2", "q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/u1jj5ykcr6xo2ie2") or die(mysqli_error());
+mysqli_connect("q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306", "l0eu1g9mu7b750ur", "ynq3czogl6mtuao2", "u1jj5ykcr6xo2ie2") or die(mysqli_error());
 echo "Connected to MySQL<br />";
 ?>
