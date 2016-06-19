f1cd244c687c43c223e851845980821711922356
Fernando <fernando.debrando@gmail.com>
2016-06-19 15:09:44 -0300

    Adicionando conexão com MySQL

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
 <?php
-phpinfo();
+mysqli_connect("l0eu1g9mu7b750ur", "ynq3czogl6mtuao2", "q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/u1jj5ykcr6xo2ie2") or die(mysqli_error());
+echo "Connected to MySQL<br />";
 ?>
