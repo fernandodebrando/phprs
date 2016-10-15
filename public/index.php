c27967d576aa22f0e3717d6c1aa5a8812425323c
Fernando Silva <fernando.debrando@gmail.com>
2016-10-14 23:47:28 -0300

    alterada mensagem de conexão

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
 echo '<h2>Hospedando seu projeto PHP no Heroku</h2>';
 mysqli_connect("q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306", "l0eu1g9mu7b750ur", "ynq3czogl6mtuao2", "u1jj5ykcr6xo2ie2") or die(mysqli_error());
-echo "Connected to MySQL<br />";
+echo "Conectado com sucesso ao banco de dados MySQL<br />";
 ?>
